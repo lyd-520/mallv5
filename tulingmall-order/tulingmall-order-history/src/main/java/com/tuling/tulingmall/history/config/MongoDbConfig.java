d31fad2aa9f4168901dce922fca4efc2e203fc5c
USER-20221017CE\Administrator <roykingw@163.com>
2022-11-01 12:14:54 +0800

    初始化提交

---
+++ tulingmall-order/tulingmall-order-history/src/main/java/com/tuling/tulingmall/history/config/MongoDbConfig.java
@@ -0,0 +1,33 @@
+package com.tuling.tulingmall.history.config;
+
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadPreference;
+import com.mongodb.TransactionOptions;
+import com.mongodb.WriteConcern;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class MongoDbConfig {
+
+//    @Bean("mongoTransactionManager")
+//    MongoTransactionManager transactionManager(MongoDatabaseFactory factory){
+//        return new MongoTransactionManager(factory);
+//    }
+
+    @Bean
+    MongoTransactionManager transactionManager(MongoDatabaseFactory factory){
+        //事务操作配置
+        TransactionOptions txnOptions = TransactionOptions.builder()
+                .readPreference(ReadPreference.primary())
+                .readConcern(ReadConcern.MAJORITY)
+                .writeConcern(WriteConcern.MAJORITY)
+                .build();
+        return new MongoTransactionManager(factory,txnOptions);
+    }
+
+}
